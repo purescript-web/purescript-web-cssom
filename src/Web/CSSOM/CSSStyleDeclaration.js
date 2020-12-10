1253cf7361fc9ff9ba837791bc7b9a2b02f11262
Thomas Honeyman <admin@thomashoneyman.com>
2020-12-10 15:06:21 -0800

    Apply eslint suggestions

--- src/Web/CSSOM/CSSStyleDeclaration.js
+++ src/Web/CSSOM/CSSStyleDeclaration.js
@@ -1,53 +1,53 @@
 "use strict";
 
-exports.cssText = function(style) {
-  return function() {
+exports.cssText = function (style) {
+  return function () {
     return style.cssText;
   };
 };
 
-exports.setCssText = function(style) {
-  return function(newCSS) {
-    return function() {
+exports.setCssText = function (style) {
+  return function (newCSS) {
+    return function () {
       style.cssText = newCSS;
     };
   };
 };
 
-exports.length = function(style) {
-  return function() {
+exports.length = function (style) {
+  return function () {
     return style.length;
   };
 };
 
-exports.getPropertyPriority = function(style) {
-  return function(propName) {
-    return function() {
+exports.getPropertyPriority = function (style) {
+  return function (propName) {
+    return function () {
       return style.getPropertyPriority(propName);
     };
   };
 };
 
-exports.getPropertyValue = function(style) {
-  return function(propName) {
-    return function() {
+exports.getPropertyValue = function (style) {
+  return function (propName) {
+    return function () {
       return style.getPropertyValue(propName);
     };
   };
 };
 
-exports.removeProperty = function(style) {
-  return function(propName) {
-    return function() {
+exports.removeProperty = function (style) {
+  return function (propName) {
+    return function () {
       style.removeProperty(propName);
     };
   };
 };
 
-exports.setProperty = function(style) {
-  return function(propName) {
-    return function(propValue) {
-      return function() {
+exports.setProperty = function (style) {
+  return function (propName) {
+    return function (propValue) {
+      return function () {
         style.setProperty(propName, propValue);
       };
     };
